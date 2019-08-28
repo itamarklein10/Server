bfed86be7b3e3f2aedcaef24e3720393a2536240
Itamar Klein <itamarklein10@gmail.com>
2019-08-28 20:50:04 +0300

    t

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -1,11 +1,9 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-using namespace std;
-// Don't forget to include "Ws2_32.lib" in the library list.
-#include <winsock2.h>
-#include <string.h>
-#include <time.h>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+
+#include "httpParser.h"
+
 
 struct SocketState
 {
@@ -13,19 +11,9 @@ struct SocketState
 	int	recv;			// Receiving?
 	int	send;			// Sending?
 	int sendSubType;	// Sending sub-type
-	char buffer[128];
+	char buffer[512];
 	int len;
 };
-
-const int TIME_PORT = 27015;
-const int MAX_SOCKETS = 60;
-const int EMPTY = 0;
-const int LISTEN = 1;
-const int RECEIVE = 2;
-const int IDLE = 3;
-const int SEND = 4;
-const int SEND_TIME = 1;
-const int SEND_SECONDS = 2;
 
 bool addSocket(SOCKET id, int what);
 void removeSocket(int index);
@@ -35,7 +23,7 @@ void sendMessage(int index);
 
 struct SocketState sockets[MAX_SOCKETS] = { 0 };
 int socketsCount = 0;
-
+struct timeval timeout;
 
 void main()
 {
@@ -127,6 +115,8 @@ void main()
 	}
 	addSocket(listenSocket, LISTEN);
 
+	timeout.tv_sec = 120;
+	timeout.tv_usec = 0;
 	// Accept connections and handles them one by one.
 	while (true)
 	{
@@ -158,14 +148,13 @@ void main()
 		// And as written above the last is a timeout, hence we are blocked if nothing happens.
 		//
 		int nfd;
-		nfd = select(0, &waitRecv, &waitSend, NULL, NULL);
+		nfd = select(0, &waitRecv, &waitSend, NULL, &timeout);
 		if (nfd == SOCKET_ERROR)
 		{
 			cout << "Time Server: Error at select(): " << WSAGetLastError() << endl;
 			WSACleanup();
 			return;
 		}
-
 		for (int i = 0; i < MAX_SOCKETS && nfd > 0; i++)
 		{
 			if (FD_ISSET(sockets[i].id, &waitRecv))
@@ -216,10 +205,15 @@ bool addSocket(SOCKET id, int what)
 			sockets[i].send = IDLE;
 			sockets[i].len = 0;
 			socketsCount++;
-			return (true);
+			if (setsockopt(id, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) == SOCKET_ERROR) {
+				cout << "Time Server: Error at setsockopt: " << WSAGetLastError() << endl;
+				return false;
+			}
+
+			return true;
 		}
 	}
-	return (false);
+	return false;
 }
 
 void removeSocket(int index)
@@ -269,7 +263,12 @@ void receiveMessage(int index)
 
 	if (SOCKET_ERROR == bytesRecv)
 	{
-		cout << "Time Server: Error at recv(): " << WSAGetLastError() << endl;
+		if (WSAGetLastError() == WSAETIMEDOUT) {
+			cout << "Time Server: Error at recv(): Too long to receive, time out triggered." << endl;
+		}
+		else {
+			cout << "Time Server: Error at recv(): " << WSAGetLastError() << endl;
+		}
 		closesocket(msgSocket);
 		removeSocket(index);
 		return;
@@ -284,66 +283,62 @@ void receiveMessage(int index)
 	{
 		sockets[index].buffer[len + bytesRecv] = '\0'; //add the null-terminating to make it a string
 		cout << "Time Server: Recieved: " << bytesRecv << " bytes of \"" << &sockets[index].buffer[len] << "\" message.\n";
-
 		sockets[index].len += bytesRecv;
-
+		string req, body;
+		map<string, string> reqContent;
 		if (sockets[index].len > 0)
 		{
-			if (strncmp(sockets[index].buffer, "TimeString", 10) == 0)
+			parseHttpRequest(sockets[index].buffer, &sockets[index].len, req, reqContent, body);
+			sockets[index].len = 0;
+			strcpy(sockets[index].buffer, "\0");
+			sockets[index].send = SEND;
+			try
 			{
-				sockets[index].send = SEND;
-				sockets[index].sendSubType = SEND_TIME;
-				memcpy(sockets[index].buffer, &sockets[index].buffer[10], sockets[index].len - 10);
-				sockets[index].len -= 10;
-				return;
+				sockets[index].sendSubType = parseHttpMethod(req);
 			}
-			else if (strncmp(sockets[index].buffer, "SecondsSince1970", 16) == 0)
+			catch (const char* exp)
 			{
-				sockets[index].send = SEND;
-				sockets[index].sendSubType = SEND_SECONDS;
-				memcpy(sockets[index].buffer, &sockets[index].buffer[16], sockets[index].len - 16);
-				sockets[index].len -= 16;
-				return;
+				sockets[index].sendSubType = ERROR_REQUEST;
 			}
-			else if (strncmp(sockets[index].buffer, "Exit", 4) == 0)
+			string ans;
+
+			if (sockets[index].sendSubType == GET)
+				ans = GetAnswer(req);
+			else if (sockets[index].sendSubType == PUT)
+				ans = PutAnswer(req, body);
+			else if (sockets[index].sendSubType == OPTIONS)
+				ans = OptionsAnswer();
+			else if (sockets[index].sendSubType == HEAD)
+				ans = HeadAnswer(req);
+			else if (sockets[index].sendSubType == _DELETE)
+				ans = DeleteAnswer(req);
+			else if (sockets[index].sendSubType == TRACE)
+				ans = TraceAnswer(req);
+			else if (sockets[index].sendSubType == EXIT)
 			{
 				closesocket(msgSocket);
 				removeSocket(index);
 				return;
 			}
+			else
+			{
+				ans = req + " method not supported.";
+			}
+
+			for (int i = 0; i < ans.size(); ++i) sockets[index].buffer[i] = ans[i];
+			sockets[index].buffer[ans.size()] = 0;
+			return;
 		}
 	}
-
 }
 
 void sendMessage(int index)
 {
 	int bytesSent = 0;
-	char sendBuff[255];
+	char sendBuff[256];
 
 	SOCKET msgSocket = sockets[index].id;
-	if (sockets[index].sendSubType == SEND_TIME)
-	{
-		// Answer client's request by the current time string.
-
-		// Get the current time.
-		time_t timer;
-		time(&timer);
-		// Parse the current time to printable string.
-		strcpy(sendBuff, ctime(&timer));
-		sendBuff[strlen(sendBuff) - 1] = 0; //to remove the new-line from the created string
-	}
-	else if (sockets[index].sendSubType == SEND_SECONDS)
-	{
-		// Answer client's request by the current time in seconds.
-
-		// Get the current time.
-		time_t timer;
-		time(&timer);
-		// Convert the number to string.
-		_itoa((int)timer, sendBuff, 10);
-	}
-
+	strcpy(sendBuff, sockets[index].buffer);
 	bytesSent = send(msgSocket, sendBuff, (int)strlen(sendBuff), 0);
 	if (SOCKET_ERROR == bytesSent)
 	{
@@ -354,4 +349,229 @@ void sendMessage(int index)
 	cout << "Time Server: Sent: " << bytesSent << "\\" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
 
 	sockets[index].send = IDLE;
+}
+
+void parseHttpRequest(char* recvBuff, int* buffSize, string& req, map<string, string>& content, string& body)
+{
+	string tempBuff(recvBuff);
+	req = getLine(tempBuff);
+	string currLine = getLine(tempBuff);
+	while (currLine.size() > 0)
+	{
+		content.insert(parseHttpLine(currLine));
+		currLine = getLine(tempBuff);
+	}
+	body.append(tempBuff);
+}
+
+string getLine(string& buffer)
+{
+	string resultString(buffer);
+	if (buffer.find('\n') == string::npos)
+	{
+		buffer = "\0";
+		return resultString;
+	}
+
+	resultString = buffer.substr(0, buffer.find('\n'));
+	buffer = buffer.substr(buffer.find('\n') + 1, buffer.size());
+	return resultString;
+}
+
+pair<string, string> parseHttpLine(string line)
+{
+	pair<string, string> reqCont;
+	if (line.find(':') != string::npos)
+	{
+		reqCont.first = line.substr(0, line.find(':'));
+		if (line.find(' ') != string::npos)
+			reqCont.second = line.substr(line.find(' '), line.size());
+		else
+			reqCont.second = "";
+	}
+	else
+	{
+		reqCont.first = line; reqCont.second = "";
+	}
+
+	return reqCont;
+}
+
+int parseHttpMethod(string request)
+{
+	int result = -1;
+	if (request.compare("exit") == 0)
+		return EXIT;
+
+	string requestType = request.substr(0, request.find(' '));
+	if (requestType.compare("GET") == 0)
+		result = GET;
+
+	if (requestType.compare("PUT") == 0)
+		result = PUT;
+
+	if (requestType.compare("DELETE") == 0)
+		result = _DELETE;
+
+	if (requestType.compare("OPTIONS") == 0)
+		result = OPTIONS;
+
+	if (requestType.compare("HEAD") == 0)
+		result = HEAD;
+
+	if (requestType.compare("TRACE") == 0)
+		result = TRACE;
+
+	return result;
+}
+
+string HeaderToSend(const string& statusMessage)
+{
+	string response = statusMessage;
+	response += "\nServer: Itamar-Shay Server\nContent-Type: text/html\nContent-length: ";
+	return response;
+}
+
+string GetFileContent(string request)
+{
+	request = request.substr(request.find(' ') + 1, request.size());
+	string fileName = request.substr(0, request.find(' '));
+	if (fileName[0] == '/')
+		fileName = fileName.substr(1, fileName.size());
+	string content;
+	if (fileName.size() > 1)
+	{
+		ifstream file;
+		file.open(fileName);
+		if (file.fail()) return "File not found";
+		char tav = file.get();
+		while (tav != EOF)
+		{
+			content.push_back(tav);
+			tav = file.get();
+		}
+	}
+	else content = "<!DOCTYPE HTML>\n<html><p>Itamar-Shay Server</p></html>";
+	return content;
+}
+
+string writeToFile(string body, string fileName)
+{
+	if (fileName[0] == '/') fileName = fileName.substr(1, fileName.size());
+	ofstream outputFile;
+	outputFile.open(fileName);
+	if (outputFile.is_open())
+	{
+		outputFile << body;
+		outputFile.close();
+		return "HTTP/1.1 201 Created";
+	}
+	else
+	{
+		return "HTTP/1.1 500 Internal Server Error";
+	}
+}
+
+string deleteFile(string fileName)
+{
+	if (fileName[0] == '/')
+		fileName = fileName.substr(1, fileName.size());
+
+	FILE* file = fopen(fileName.c_str(), "r");
+	if (file != nullptr)
+	{
+		fclose(file);
+		if (remove(fileName.c_str()) == 0)
+		{
+			return "HTTP/1.1 200 OK";
+		}
+		else
+		{
+			return "HTTP/1.1 500 Internal Server Error";
+		}
+	}
+	else
+	{
+		return "HTTP/1.1 404 Not Found";
+	}
+}
+
+
+string GetAnswer(string request)
+{
+	string body = GetFileContent(request);
+	string messageStatus = "HTTP/1.1 ";
+	if (body.compare("File not found") == 0)
+		messageStatus += "404 Not Found";
+	else
+		messageStatus += "200 OK";
+	string httpHeader = HeaderToSend(messageStatus);
+	char* MessageSize = new char[8];
+	_itoa(body.size(), MessageSize, 10);
+	httpHeader += MessageSize;
+	delete[]MessageSize;
+	return httpHeader + "\n\n" + body;
+}
+
+string PutAnswer(string request, string body)
+{
+	string requestSub = request.substr(request.find(' ') + 1, request.size());
+	string fileName;
+	if (requestSub.find(' ') == string::npos)
+		fileName = requestSub;
+	else
+		fileName = requestSub.substr(0, requestSub.find(' '));
+
+	string messageStatus = writeToFile(body, fileName);
+	string header = HeaderToSend(messageStatus);
+	header += "0\n\n";
+	return header;
+}
+
+string DeleteAnswer(string request)
+{
+	string requestSub = request.substr(request.find(' ') + 1, request.size());
+	string fileName = requestSub.substr(0, requestSub.find(' '));
+	string statusMess = deleteFile(fileName);
+	string header = HeaderToSend(statusMess);
+	string body;
+	if (statusMess.find("200") != string::npos)
+		body = "<html><h1>URL deleted.</h1></html>";
+	else
+		body = "<html><h1>URL not deleted.</h1></html>";
+	header.append(to_string(body.size()));
+	return header + "\n\n" + body;
+}
+
+string OptionsAnswer()
+{
+	string returnMessage = HeaderToSend("HTTP/1.1 204 No Content");
+	string methodsAllowed = "Access-Control-Allow-Methods: GET, PUT, OPTIONS, TRACE, HEAD, DELETE, Exit";
+	returnMessage += "0";
+	returnMessage += "\n" + methodsAllowed + "\n\n";
+	return returnMessage;
+}
+
+string TraceAnswer(string request)
+{
+	string answer("HTTP/1.1 200 OK");
+	answer += "\nServer: Itamar-Shay Server\nContent-Type: message/http";
+	return answer;
+}
+
+string HeadAnswer(string request)
+{
+	string body = GetFileContent(request);
+	string messageStatus = "HTTP/1.1 ";
+	if (body.compare("File not found") == 0)
+		messageStatus.append("404 Not Found");
+	else
+		messageStatus.append("200 OK");
+
+	string header = HeaderToSend(messageStatus);
+	char* MessageSize = new char[8];
+	_itoa(body.size(), MessageSize, 10);
+	header += MessageSize;
+	delete[]MessageSize;
+	return header;
 }
---
+++ Server/httpParser.h
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef HTTP_PARSER
+#define HTTP_PARSER
+
+#include <stdio.h>
+#include <winsock2.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+const int TIME_PORT = 27015;
+const int MAX_SOCKETS = 60;
+const int EMPTY = 0;
+const int LISTEN = 1;
+const int RECEIVE = 2;
+const int IDLE = 3;
+const int SEND = 4;
+const int GET = 5;
+const int PUT = 6;
+const int OPTIONS = 7;
+const int TRACE = 8;
+const int HEAD = 9;
+const int _DELETE = 10;
+const int EXIT = 11;
+const int ERROR_REQUEST = -1;
+
+void parseHttpRequest(char* recvBuff, int* buffSize, string& req, map<string, string>& content, string& body);
+string getLine(string& buff);
+pair<string, string> parseHttpLine(string line);
+string GetAnswer(string request);
+int parseHttpMethod(string request);
+string PutAnswer(string request, string body);
+string DeleteAnswer(string request);
+string OptionsAnswer();
+string TraceAnswer(string request);
+string HeadAnswer(string request);
+string GetFileContent(string request);
+string writeToFile(string body, string fileName);
+string deleteFile(string fileName);
+
+
+
+#endif
